4a8c0b27d6c3c1f2bd76868ba0b5f1802b8d7466
karim-aziz52 <karim.aziz@argaam.com>
2022-07-18 10:12:50 +0200

    Add AR Map

--- src/Components/ServicesSolutionsComponents/MapSection/MapSection.js
+++ src/Components/ServicesSolutionsComponents/MapSection/MapSection.js
@@ -5,17 +5,24 @@ import { useTranslation } from "react-i18next";
 import "./MapSection.css";
 // Import Img
 import mapImg from "../../../Assets/servicesSolutions/map.png";
+import mapImgAr from "../../../Assets/servicesSolutions/mapAr.png";
 
 const MapSection = () => {
   // Import Use Translation
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <div className="map-section">
       <div className="h3">{t("servicesSolutions.mapT")}</div>
 
-      <div className="mab-box">
+      {/* Map En */}
+      <div className={i18n.language === "ar" ? "d-n" : "mab-box"}>
         <img className="img-fluid" src={mapImg} alt="Map" />
+      </div>
+
+      {/* Map Ar */}
+      <div className={i18n.language === "en" ? "d-n" : "mab-box"}>
+        <img className="img-fluid" src={mapImgAr} alt="Map" />
       </div>
     </div>
   );
